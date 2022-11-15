840cbd82255fe196b6e6beb93da437553b837880
Chris Lynch <masterninjadog01@gmail.com>
2022-11-14 20:06:13 -0600

    week 2 task 1 complete

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
@@ -7,9 +8,9 @@ import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
+    const [showModal, setShowModal] = useState(false);
     const dispatch = useDispatch();
 
     const renderCommentItem = ({ item }) => {
@@ -25,6 +26,7 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
+        <>
         <FlatList
             data={comments.commentsArray.filter(
                 (comment) => comment.campsiteId === campsite.id
@@ -41,11 +43,29 @@ const CampsiteInfoScreen = ({ route }) => {
                         campsite={campsite}
                         isFavorite={favorites.includes(campsite.id)}
                         markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                        onShowModal={() => setShowModal(!showModal)}
                     />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
             }
         />
+        <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+            <View style={styles.modal}>
+                <View style={{margin: 10}}>
+                    <Button
+                        onPress={() => setShowModal(!showModal)}
+                        color='#808080'
+                        title='Cancel'
+                    />
+                </View>
+            </View>
+            </Modal>
+                     </>
     );
 };
 
@@ -63,6 +83,10 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
     }
 });
 
