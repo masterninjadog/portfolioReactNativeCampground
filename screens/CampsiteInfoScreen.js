a5bfe409eada921bfb6665c33538a8f3924b64c4
Chris Lynch <masterninjadog01@gmail.com>
2022-11-15 21:06:38 -0600

    workshop2Task2Complete

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,23 +1,47 @@
 import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
+import { Rating, Input } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
-
-
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
     const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState("");
+    const [text, setText] = useState("");
     const dispatch = useDispatch();
+
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+        console.log(newComment);
+        setShowModal(!showModal)
+    } 
+
+    const resetForm = () => {
+        setRating(5)
+        setAuthor("")
+        setText("")
+    }
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Rating
+                    readonly
+                    startingValue={rating}
+                    imageSize={10}
+                    style={{ alignItems: 'flex-start', paddingVertical: '5%' }}
+                />
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
@@ -49,23 +73,56 @@ const CampsiteInfoScreen = ({ route }) => {
                 </>
             }
         />
-        <Modal
+            <Modal
                 animationType='slide'
                 transparent={false}
                 visible={showModal}
                 onRequestClose={() => setShowModal(!showModal)}
             >
-            <View style={styles.modal}>
-                <View style={{margin: 10}}>
-                    <Button
-                        onPress={() => setShowModal(!showModal)}
-                        color='#808080'
-                        title='Cancel'
+                <View style={styles.modal}>
+                    <Rating
+                    showRating
+                    startingValue={rating}
+                    imageSize={40}
+                    onFinishRating={(rating)=> setRating(rating)}
+                    style={{paddingVertical: 10}}
                     />
+                    <Input
+                    placeholder='Author'
+                    leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                    leftIconContainerStyle={{ paddingRight: 10 }}
+                    onChangeText={(author) => setAuthor(author)}
+                    value={author}
+                    />
+                    <Input
+                    placeholder='Comment'
+                    leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                    leftIconContainerStyle={{ paddingRight: 10 }}
+                    onChangeText={(text) => setText(text)}
+                    value={text}
+                    />
+                    <View style={{margin: 10}}>
+                        <Button 
+                            onPress={() => {
+                                resetForm();
+                            }}
+                            color="#5637DD"
+                            title="Submit"
+                        />
+                    </View>
+                    <View style={{margin: 10}}>
+                        <Button
+                            onPress={() => {
+                                setShowModal(!showModal)
+                                resetForm();
+                            }}
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>
                 </View>
-            </View>
             </Modal>
-                     </>
+        </>  
     );
 };
 
